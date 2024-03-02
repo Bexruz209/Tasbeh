4d23eb1f23a364ed0043b844482874e48d0b579e
Bexruz209 <b7079921@gmail.com>
2024-03-02 13:47:16 +0500

    Tasbex

---
+++ index.js
@@ -0,0 +1,18 @@
+let ekran = document.querySelector('.ekran');
+let button = document.querySelector('.emmer__button');
+let buton = document.querySelector('.emmer_button')
+
+let i = 0;
+let a = 0;
+
+button.onclick = () => {
+    ekran.innerHTML = i += 1
+    if (ekran.innerHTML == 100) {
+        alert('Siz 99 zikr aytiz')
+        ekran.innerHTML = i = 0
+    }
+}
+
+buton.onclick = () => {
+    ekran.innerHTML = i = 0
+}
